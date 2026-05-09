05847fea56cff8d5de48978e7953b3b8cbe46e33
akash-0905 <vsakash057@gmail.com>
2026-05-09 21:29:20 +0530

    Updated BikeRentalManagement System

---
+++ src/Exception/CustomerAlreadyExistedException.java
@@ -0,0 +1,8 @@
+package Exception;
+
+public class CustomerAlreadyExistedException extends Exception{
+
+    public CustomerAlreadyExistedException(String message) {
+        super(message);
+    }
+}
---
+++ src/Service/CustomerService.java
@@ -0,0 +1,12 @@
+package Service;
+
+import Model.Customer;
+
+import java.util.ArrayList;
+
+public class CustomerService {
+
+        private ArrayList<Customer> customers = new ArrayList<>();
+
+    }
+
