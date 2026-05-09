20eac4059d042d6a98881ce06c5adf9496557c2d
akash-0905 <vsakash057@gmail.com>
2026-05-09 21:43:36 +0530

    Updated CustomerService

--- src/Service/CustomerService.java
+++ src/Service/CustomerService.java
@@ -1,12 +1,38 @@
 package Service;
 
 import Model.Customer;
+import Exception.CustomerAlreadyExistedException;
+import Exception.CustomerNotFoundException;
 
 import java.util.ArrayList;
 
 public class CustomerService {
 
         private ArrayList<Customer> customers = new ArrayList<>();
+
+        public void addCustomer(Customer customer) throws CustomerAlreadyExistedException {
+
+            for(Customer existingCustomer : customers){
+
+                if (existingCustomer.getCustomerId() == customer.getCustomerId()) {
+
+                    throw new CustomerAlreadyExistedException("Customer Already Exists");
+                }
+            }
+            customers.add(customer);
+            System.out.println("Customer Added Successfully");
+        }
+
+        public void viewCustomer() throws CustomerNotFoundException{
+
+            if (customers.isEmpty()) {
+                throw new CustomerNotFoundException("No Customers Found");
+            }
+
+            for (Customer customer : customers) {
+                System.out.println(customer);
+            }
+        }
 
     }
 
