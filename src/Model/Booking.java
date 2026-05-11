92051654b71f651050805b4b6b6609667ae832c2
akash-0905 <vsakash057@gmail.com>
2026-05-11 08:02:53 +0530

    Booking Parameters added

--- src/Model/Booking.java
+++ src/Model/Booking.java
@@ -1,4 +1,51 @@
 package Model;
 
+import java.time.LocalDate;
+
 public class Booking {
+    private int bookingId;
+
+    private Customer customer;
+
+    private Bike bike;
+
+    private LocalDate bookingDate;
+
+    public Booking(int bookingId,
+                   Customer customer,
+                   Bike bike,
+                   LocalDate bookingDate) {
+
+        this.bookingId = bookingId;
+        this.customer = customer;
+        this.bike = bike;
+        this.bookingDate = bookingDate;
+    }
+
+    public int getBookingId() {
+        return bookingId;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public Bike getBike() {
+        return bike;
+    }
+
+    public LocalDate getBookingDate() {
+        return bookingDate;
+    }
+
+    @Override
+    public String toString() {
+
+        return "Booking{" +
+                "bookingId=" + bookingId +
+                ", customer=" + customer.getCustomerName() +
+                ", bike=" + bike.getBrand() +
+                ", bookingDate=" + bookingDate +
+                '}';
+    }
 }
