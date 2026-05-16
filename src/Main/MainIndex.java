3dd6928ba2a155a2a4fc9d5389cb31290bb8a74c
akash-0905 <vsakash057@gmail.com>
2026-05-16 09:48:55 +0530

    Added WishList like MenuCard

--- src/Main/MainIndex.java
+++ src/Main/MainIndex.java
@@ -2,89 +2,47 @@ package Main;
 
 import Exception.BikeAlreadyExistedException;
 import Exception.BikesnotAvailableException;
-import Exception.BikeNotFoundException;
-import Exception.BikeReturnedAvaialbleException;
-import Exception.CustomerAlreadyExistedException;
-import Exception.CustomerNotFoundException;
 import Model.Bike;
-import Model.Booking;
-import Model.Customer;
 import Service.BikeService;
-import Service.BookingService;
-import Service.CustomerService;
 
-import java.time.LocalDate;
-
+import java.util.Scanner;
 
 public class MainIndex {
-    static void main(String[] args) throws BikesnotAvailableException, BikeNotFoundException {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
 
         BikeService bikeService = new BikeService();
-        CustomerService customerService = new CustomerService();
-        BookingService bookingService = new BookingService();
-
-        System.out.println("Adding Bike:");
-        Bike bike1 = new Bike(101, "Royal GT", "GT650", 650, true);
-        Bike bike2 = new Bike(102, "Duke", "Duke350Ktm", 1500, true);
-        Bike bike3 = new Bike(103, "HeroHonda", "Splendor", 500, false);
-
-        Customer customer1 = new Customer(1, "Akash", "9876543210");
-        Customer customer2 = new Customer(2, "Vijay", "9876543211");
-        Customer customer3 = new Customer(3, "Vishnu", "9632581470");
-
-
-        Booking booking1 = new Booking(1, customer1, bike1, LocalDate.now());
-        Booking booking2 = new Booking(2, customer2, bike2, LocalDate.now());
-        Booking booking3 = new Booking(3, customer3, bike3, LocalDate.now());
-
-        bookingService.addBooking(booking1);
-        bookingService.addBooking(booking2);
-        bookingService.addBooking(booking3);
-        bookingService.viewBookings();
 
         try {
-            bikeService.addBike(bike1);
-            bikeService.addBike(bike2);
-//            bikeService.addBike(bike1);
-            bikeService.addBike(bike3);
-            bikeService.rentBike(115);
+            System.out.println("Enter Bike ID:");
+
+            int id = sc.nextInt();
+
+            System.out.println("Enter Brand:");
+            String brand = sc.next();
+
+            System.out.println("Enter Model:");
+
+            String model = sc.next();
+
+            System.out.println(
+                    "Enter Rent Per Day:"
+            );
+
+            double rent = sc.nextDouble();
+
+            Bike bike = new Bike(id, brand, model, rent, true);
+
+            bikeService.addBike(bike);
+
+            bikeService.viewBike();
         }
-        catch (BikeAlreadyExistedException | BikesnotAvailableException | BikeNotFoundException e){
+
+        catch (BikeAlreadyExistedException | BikesnotAvailableException e) {
+
             System.out.println(e.getMessage());
         }
-
-       try{
-           bikeService.viewBike();
-       }
-       catch (BikesnotAvailableException e){
-           System.out.println(e.getMessage());
-       }
-
-       try{
-           bikeService.returnBike(101);
-           bikeService.returnBike(101);
-       } catch (BikeReturnedAvaialbleException e) {
-           System.out.println(e.getMessage());
-       }
-
-        try {
-            customerService.addCustomer(customer1);
-            customerService.addCustomer(customer2);
-            customerService.addCustomer(customer1);
-            customerService.viewCustomer();
-        } catch (CustomerAlreadyExistedException | CustomerNotFoundException e) {
-            System.out.println(e.getMessage());
-        }
-
-        bikeService.removeBike(103);
-
-        bikeService.updateBikeRent(101, 2000);
-
-        bikeService.viewBike();
-
     }
 }
-
-
-
-
--- src/Service/BikeService.java
+++ src/Service/BikeService.java
@@ -12,7 +12,6 @@ public class BikeService {
         ArrayList<Bike> bikes = new ArrayList<>();
 
         public void addBike(Bike bike) throws BikeAlreadyExistedException {
-
             if(bikes.contains(bike)){
                 throw new BikeAlreadyExistedException("Bike is Already Existed");
             }else{
@@ -22,7 +21,6 @@ public class BikeService {
         }
 
         public void viewBike() throws BikesnotAvailableException {
-
         if(bikes.isEmpty()){
             throw new BikesnotAvailableException("Bike not Available");
         }
@@ -33,11 +31,9 @@ public class BikeService {
 
         public void rentBike(int bikeId) throws BikesnotAvailableException,
                 BikeNotFoundException {
-
             boolean found = false;
 
             for(Bike bike : bikes){
-
                 if(bike.getBikeId() == bikeId){
                     found = true;
 
@@ -154,7 +150,7 @@ public class BikeService {
             if (bike.getBikeId() == bikeId) {
                 found = true;
                 bike.setRentPerDay(newRentPerDay);
-                System.out.println("Bike Rent Updated ✅");
+                System.out.println("Bike Rent Updated");
             }
         }
         if (!found) {
