affc75ae7c470ffe32cdd0decbf665776ef37915
akash-0905 <vsakash057@gmail.com>
2026-05-16 09:58:43 +0530

    updated by Scanner

--- src/Main/MainIndex.java
+++ src/Main/MainIndex.java
@@ -2,6 +2,8 @@ package Main;
 
 import Exception.BikeAlreadyExistedException;
 import Exception.BikesnotAvailableException;
+import Exception.BikeNotFoundException;
+import Exception.BikesnotAvailableException;
 import Model.Bike;
 import Service.BikeService;
 
@@ -15,34 +17,85 @@ public class MainIndex {
 
         BikeService bikeService = new BikeService();
 
-        try {
-            System.out.println("Enter Bike ID:");
+        while (true) {
 
-            int id = sc.nextInt();
+            System.out.println("\n===== Bike Rental System =====");
 
-            System.out.println("Enter Brand:");
-            String brand = sc.next();
+            System.out.println("1. Add Bike");
+            System.out.println("2. View Bikes");
+            System.out.println("3. Rent Bike");
+            System.out.println("4. Exit");
 
-            System.out.println("Enter Model:");
+            System.out.println("\nEnter Your Choice:");
 
-            String model = sc.next();
+            int choice = sc.nextInt();
 
-            System.out.println(
-                    "Enter Rent Per Day:"
-            );
+            switch (choice) {
 
-            double rent = sc.nextDouble();
+                case 1:
 
-            Bike bike = new Bike(id, brand, model, rent, true);
+                    try {
 
-            bikeService.addBike(bike);
+                        System.out.println("\nEnter Bike ID:");
+                        int id = sc.nextInt();
 
-            bikeService.viewBike();
-        }
+                        System.out.println("Enter Brand:");
+                        String brand = sc.next();
 
-        catch (BikeAlreadyExistedException | BikesnotAvailableException e) {
+                        System.out.println("Enter Model:");
+                        String model = sc.next();
 
-            System.out.println(e.getMessage());
+                        System.out.println("Enter Rent Per Day:");
+                        double rent = sc.nextDouble();
+
+                        Bike bike = new Bike(id, brand, model, rent, true);
+
+                        bikeService.addBike(bike);
+                    }
+
+                    catch (BikeAlreadyExistedException e) {
+                        System.out.println(e.getMessage());
+                    }
+
+                    break;
+
+                case 2:
+
+                    try {
+                        bikeService.viewBike();
+                    }
+                    catch (BikesnotAvailableException e) {
+                        System.out.println(e.getMessage());
+                    }
+
+                    break;
+
+                case 3:
+
+                    try {
+                        System.out.println("\nEnter Bike ID to Rent:");
+
+                        int bikeId = sc.nextInt();
+                        bikeService.rentBike(bikeId);
+                    }
+
+                    catch (BikeNotFoundException | BikesnotAvailableException e) {
+
+                        System.out.println(e.getMessage());
+                    }
+
+                    break;
+
+                case 4:
+
+                    System.out.println("Exiting Application");
+
+                    return;
+
+                default:
+
+                    System.out.println("Invalid Choice");
+            }
         }
     }
 }
